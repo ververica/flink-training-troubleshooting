a9f9f9916a1eef46f68d85336d158b886776a93b
Konstantin Knauf <konstantin.knauf@tngtech.com>
2018-08-23 14:05:12 +0200

    added mc binary to upload jars to mino

--- bin/create_deployment.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo "This will create the deployment at $1"
--- bin/upload_artifact.sh
+++ bin/upload_artifact.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-echo "This will upload the artifact to $1"
+
+HOST=$1
+
+${0%/*}/mc config host add minio http://$HOST:30001 admin password
+${0%/*}/mc cp ${0%/*}/../target/flink-training-troubleshooting-0.1.jar minio/flink
