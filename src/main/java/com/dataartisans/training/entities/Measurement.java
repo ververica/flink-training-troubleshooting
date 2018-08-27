2cf1006f238c5d814f00ca3bbbd8327fc6a12e96
Konstantin Knauf <konstantin.knauf@tngtech.com>
2018-08-27 13:53:46 +0200

    added temperature field to measurement dto

--- src/main/java/com/dataartisans/training/entities/Measurement.java
+++ src/main/java/com/dataartisans/training/entities/Measurement.java
@@ -12,5 +12,11 @@ public class Measurement {
     private int    sensorId;
     private double value;
     private String location;
+    private float  temperature;
 
+    public Measurement(final int sensorId, final double value, final String location) {
+        this.sensorId = sensorId;
+        this.value = value;
+        this.location = location;
+    }
 }
