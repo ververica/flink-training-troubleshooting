670d219a8a3cb1e4d7f71b2149a04b1d92204459
Konstantin Knauf <konstantin.knauf@tngtech.com>
2018-08-29 09:59:06 +0200

    S3 Upload with Maven

--- bin/upload_artifact.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-HOST=$1
-
-${0%/*}/mc config host add minio http://$HOST:30001 admin password
-${0%/*}/mc cp ${0%/*}/../target/flink-training-troubleshooting-0.1.jar minio/flink
