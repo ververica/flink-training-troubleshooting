1bfc8a01bea245d8adc2f5b62c1396ec43d07cc5
Seth Wiesman <sjwiesman@gmail.com>
2018-09-12 15:11:27 +0200

    simplify usage across IDE's and operating systems (#1)
    simplify usage across IDE's and operating systems

--- src/main/java/com/dataartisans/training/TroubledStreamingJob.java
+++ src/main/java/com/dataartisans/training/TroubledStreamingJob.java
@@ -2,12 +2,13 @@ package com.dataartisans.training;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -24,6 +25,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
 public class TroubledStreamingJob {
@@ -31,8 +33,27 @@ public class TroubledStreamingJob {
     public static void main(String[] args) throws Exception {
         ParameterTool parameters = ParameterTool.fromArgs(args);
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        final boolean local = parameters.getBoolean("local", false);
+
+        StreamExecutionEnvironment env;
+        if (local) {
+            env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+            String statePath = parameters.get("fsStatePath");
+            Path checkpointPath;
+            if (statePath != null) {
+                FileUtils.deleteDirectory(new File(new URI(statePath)));
+                checkpointPath = Path.fromLocalFile(new File(new URI(statePath)));
+            } else {
+                checkpointPath = Path.fromLocalFile(Files.createTempDirectory("checkpoints").toFile());
+            }
+
+            StateBackend stateBackend = new FsStateBackend(checkpointPath);
+            env.setStateBackend(stateBackend);
+        } else {
+            env = StreamExecutionEnvironment.getExecutionEnvironment();
+        }
+
         env.getConfig().setGlobalJobParameters(parameters);
 
         //Time Characteristics
@@ -42,13 +63,6 @@ public class TroubledStreamingJob {
         //Checkpointing Configuration
         env.enableCheckpointing(5000);
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(4000);
-        if (env instanceof LocalStreamEnvironment) {
-            // configure checkpoint storage for local executions
-            String statePath = parameters.get("fsStatePath", "file:///" + System.getProperty("user.dir") + "/chkpts");
-            FileUtils.deleteDirectory(new File(new URI(statePath))); // cleanup last executions
-            StateBackend stateBackend = new FsStateBackend(statePath);
-            env.setStateBackend(stateBackend);
-        }
 
         //Restart Strategy (always restart)
         env.setRestartStrategy(RestartStrategies.failureRateRestart(10,
@@ -75,13 +89,14 @@ public class TroubledStreamingJob {
                 .process(new MeasurementWindowAggregatingFunction())
                 .name("WindowedAggregationPerLocation");
 
-        aggregatedPerLocation.addSink(new DiscardingSink<>()).name("NormalOutput").disableChaining(); //use for performance testing in dA Platform
-//        aggregatedPerLocation.print().name("output").disableChaining(); //use for local testing
-
-        aggregatedPerLocation.getSideOutput(lateDataTag).addSink(new DiscardingSink<>()).name("LateDataSink").disableChaining();
-//        aggregatedPerLocation.getSideOutput(lateDataTag).printToErr().name("LateDataSink").disableChaining(); //use for local testing
+        if (local) {
+            aggregatedPerLocation.print().name("output").disableChaining();
+            aggregatedPerLocation.getSideOutput(lateDataTag).printToErr().name("LateDataSink").disableChaining();
+        } else {
+            aggregatedPerLocation.addSink(new DiscardingSink<>()).name("NormalOutput").disableChaining();
+            aggregatedPerLocation.getSideOutput(lateDataTag).addSink(new DiscardingSink<>()).name("LateDataSink").disableChaining();
+        }
 
         env.execute();
     }
-
 }
